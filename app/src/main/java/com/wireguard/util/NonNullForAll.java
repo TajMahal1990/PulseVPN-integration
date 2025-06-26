d7ab983535616f61fe8dec8027e3ae128b0dc8d6
eddy <eddy@workbp.club>
2025-06-26 18:34:23 +0600

    add

--- app/src/main/java/com/wireguard/util/NonNullForAll.java
+++ app/src/main/java/com/wireguard/util/NonNullForAll.java
@@ -5,25 +5,18 @@
 
 package com.wireguard.util;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-import javax.annotation.Nonnull;
-import javax.annotation.meta.TypeQualifierDefault;
 
 import androidx.annotation.RestrictTo;
 import androidx.annotation.RestrictTo.Scope;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * This annotation can be applied to a package, class or method to indicate that all
  * class fields and method parameters and return values in that element are nonnull
  * by default unless overridden.
  */
-@RestrictTo(Scope.LIBRARY_GROUP)
-@Nonnull
-@TypeQualifierDefault({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@RestrictTo(Scope.LIBRARY_GROUP_PREFIX)
 @Retention(RetentionPolicy.RUNTIME)
-
-public @interface NonNullForAll {
-}
+public @interface NonNullForAll {}
